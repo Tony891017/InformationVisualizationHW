30502d8496b934ab580de3be4cf7892c5628a274
Pei_Yun_Kuo <65301061+Tony891017@users.noreply.github.com>
2022-04-18 14:22:18 +0800

    Update HW2_script.js

--- HW2_script.js
+++ HW2_script.js
@@ -1,2 +1,2 @@
-let url = https://raw.githubusercontent.com/BigTimeStats/beach-volleyball/master/data/by_year_and_tour/"
+let url = "https://raw.githubusercontent.com/BigTimeStats/beach-volleyball/master/data/by_year_and_tour/"
 console.log(url)
